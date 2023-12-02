6ef55905c34d81a9a282e7b2c9b6217ef9fdd430
Nils <nilsdaniel.cichy@gmail.com>
2023-12-02 14:05:14 +0100

    refactor day 2 (separate cleanup from calculations) for reusability
    + make it more generic

--- 2023/02/solution.py
+++ 2023/02/solution.py
@@ -8,18 +8,12 @@ BASE_DIR = Path(__file__).resolve().parent
 
 def extract_possible_games(
     game_logs: list[str],
-    amount_red: int,
-    amount_blue: int,
-    amount_green: int,
+    amount_per_color: Mapping[str, int],
 ) -> list[int]:
     valid_games = []
     for i, game_log in enumerate(game_logs):
-        if is_game_possible(
-            game_log, amount_red=amount_red, amount_green=amount_green, amount_blue=amount_blue
-        ):
+        if is_game_possible(game_log, amount_per_color):
             valid_games.append(i + 1)
-        min_colors = extract_min_amounts(game_log)
-        set_power = min_colors[0] * min_colors[1] * min_colors[2]
 
     return valid_games
 
@@ -28,56 +22,58 @@ def extract_minimum_set_powers(game_logs: list[str]) -> list[int]:
     set_powers = []
     for i, game_log in enumerate(game_logs):
         min_colors = extract_min_amounts(game_log)
-        set_powers.append(min_colors[0] * min_colors[1] * min_colors[2])
+        power = 1
+        # now works with any amount of colors,
+        # but if one color is not needed, it will not multiply by 0
+        for amount in min_colors.values():
+            power *= amount
+        set_powers.append(power)
 
     return set_powers
 
 
-def is_game_possible(
-    game_log: str,
-    amount_red: int,
-    amount_blue: int,
-    amount_green: int,
-) -> bool:
+def get_clean_game_log(game_log: str) -> list[dict[str, int]]:
+    """Eliminates game id, splits the different pulls, and for each
+    one builds a mapping from colors to their amount"""
+    clean_pulls = []
     game_id, pulls = game_log.split(":")
-    pulls = pulls.strip().split(";")
-    color_max = {"blue": amount_blue, "green": amount_green, "red": amount_red}
+    pulls = pulls.split(";")
     for pull in pulls:
-        color_pulls = pull.strip().split(",")
+        color_pulls = pull.split(",")
+        amount_per_color = {}
         for color_pull in color_pulls:
             amount, color = color_pull.strip().split(" ")
-            if int(amount.strip()) > color_max[color]:  # raises if color not in mapping
+            amount_per_color[color] = int(amount)
+        clean_pulls.append(amount_per_color)
+    return clean_pulls
+
+
+def is_game_possible(
+    game_log: str,
+    amount_per_color: Mapping[str, int],
+) -> bool:
+    for pull in get_clean_game_log(game_log):
+        for color, amount in pull.items():
+            if amount > amount_per_color.get(color, 0):
+                # if a color was not provided in the mapping,
+                # we interpret it as not being available (amount 0)
                 return False
-            # if color == "blue" and int(amount) > amount_blue:
-            #     return False
-            # elif color == "red" and int(amount) > amount_red:
-            #     return False
-            # elif color == "green" and int(amount) > amount_green:
-            #     return False
-            # else:
-            #     raise ValueError(f"Non recognized color {color}")
+
     return True
 
 
-def extract_min_amounts(
-    game_log: str,
-) -> tuple[int, int, int]:  # r,g,b
-    game_id, pulls = game_log.split(":")
-    pulls = pulls.strip().split(";")
-    color_maxs = {"blue": 0, "green": 0, "red": 0}
-    for pull in pulls:
-        color_pulls = pull.strip().split(",")
-        for color_pull in color_pulls:
-            amount, color = color_pull.strip().split(" ")
-            amount = int(amount.strip())
-            color = color.strip()
-            if color_maxs.get(color) < amount:
+def extract_min_amounts(game_log: str) -> Mapping[str, int]:  # works with any color
+    color_maxs = {}
+    for pull in get_clean_game_log(game_log):
+        for color, amount in pull.items():
+            if color_maxs.get(color, 0) < amount:
                 color_maxs[color] = amount
-    return color_maxs["red"], color_maxs["green"], color_maxs["blue"]
+
+    return color_maxs
 
 
 @pytest.mark.parametrize(
-    ["game_logs", "amount_red", "amount_blue", "amount_green", "expected_result"],
+    ["game_logs", "amount_per_color", "expected_result"],
     [
         [
             [
@@ -87,23 +83,17 @@ def extract_min_amounts(
                 "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
                 "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
             ],
-            12,
-            13,
-            14,
+            {"blue": 12, "green": 13, "red": 14},
             [1, 2, 5],
         ],
     ],
 )
 def test_extract_valid_games(
     game_logs: list[str],
-    amount_red: int,
-    amount_blue: int,
-    amount_green: int,
+    amount_per_color: Mapping[str, int],
     expected_result: list[int],
 ):
-    v = extract_possible_games(
-        game_logs, amount_blue=amount_blue, amount_red=amount_red, amount_green=amount_green
-    )
+    v = extract_possible_games(game_logs, amount_per_color=amount_per_color)
     assert v == expected_result
 
 
@@ -112,27 +102,27 @@ def test_extract_valid_games(
     [
         [
             "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-            (4, 2, 6),
+            {"red": 4, "green": 2, "blue": 6},
         ],
         [
             "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-            (1, 3, 4),
+            {"red": 1, "green": 3, "blue": 4},
         ],
         [
             "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-            (20, 13, 6),
+            {"red": 20, "green": 13, "blue": 6},
         ],
         [
             "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-            (14, 3, 15),
+            {"red": 14, "green": 3, "blue": 15},
         ],
         [
             "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
-            (6, 3, 2),
+            {"red": 6, "green": 3, "blue": 2},
         ],
         [
             "Game 6: 1 blue, 3 green; 2 blue, 2 green",
-            (0, 3, 2),
+            {"green": 3, "blue": 2},
         ],
     ],
 )
@@ -148,16 +138,13 @@ def test_extract_minimum_colors(
 
 if __name__ == "__main__":
     with open(BASE_DIR / "input.txt", "r") as f:
-        # game_ids = extract_minimum_set_powers(
-        #     f.readlines(),
-        #     amount_red=12,
-        #     amount_green=13,
-        #     amount_blue=14,
-        # )
-        # print(game_ids)
-        # print(sum(game_ids))
-        powers = extract_minimum_set_powers(
-            f.readlines(),
+        game_ids = extract_possible_games(
+            f.readlines(), amount_per_color={"blue": 12, "green": 13, "red": 14}
         )
-        print(powers)
-        print(sum(powers))
+        print(game_ids)
+        print(sum(game_ids))
+        # powers = extract_minimum_set_powers(
+        #     f.readlines(),
+        # )
+        # print(powers)
+        # print(sum(powers))
