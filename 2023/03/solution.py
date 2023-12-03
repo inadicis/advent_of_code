30201bdc1dd9793ec44fd5cdba0ecc0ab48dab32
Nils <nilsdaniel.cichy@gmail.com>
2023-12-03 17:20:54 +0100

    implement qa function, did not find mistaken number
    seems like either I did not understand the assignment, or the assignment is wrong

--- 2023/03/solution.py
+++ 2023/03/solution.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent
 # in original input
 # - go through numbers, find out if there is a symbol adjacent
 
-def extract_safe_numbers(lines: list[str]) -> list[int]:
+def extract_safe_numbers(lines: list[str]) -> list[tuple[int, int, int]]:
+    """Returns a number, its line index and its last character char index"""
     safe_numbers = []
     # previous_line = None
     for line_index, line in enumerate(lines):
@@ -50,13 +51,15 @@ def extract_safe_numbers(lines: list[str]) -> list[int]:
 
             else:
                 if current_number and is_current_number_safe:
-                    safe_numbers.append(int(current_number))
+                    safe_numbers.append(
+                        (int(current_number), line_index, char_index)
+                    )
                 current_number = ""
                 is_current_number_safe = False
 
             # handle numbers that end the line as well
         if current_number and is_current_number_safe:
-            safe_numbers.append(int(current_number))
+            safe_numbers.append((int(current_number), line_index, len(line) - 1))
 
             # previous_char = char
 
@@ -119,44 +122,70 @@ def test_extract_safe_numbers(
         lines: list[str],
         expected_result: list[int],
 ):
-    v = extract_safe_numbers(
-        lines,
-    )
-    assert v == expected_result
+    numbers = [number for number, line, char in extract_safe_numbers(
+        lines
+    )]
+    assert numbers == expected_result
 
 
-def qa_results(lines: list[str], safe_numbers: list[int]):
-    # add points around to avoid out of bounds when looking around digits
-    # pprint.pp(lines)
-    wrapped_lines = ["." + line.strip() + "." for line in lines]
-    line_length = len(wrapped_lines[0])
-    text = "." * line_length + "\n" + "\n".join(wrapped_lines) + "\n" + "." * line_length
-    # pprint.pp(wrapped_lines)
-    # pprint.pp(text)
-    current_index = 0
-    for number_id, number in enumerate(safe_numbers):
-        sub = str(number)
-        i = text.find(sub, current_index, len(text))
-        min_col = i - 1
-        max_col = i + len(sub) + 1  # excluded
-        # print(f"{number_id=}, {number=}, {i=}, {min_col=}, {max_col=}")
-        offset = line_length + 1  # we have to count newline char now
-        chars_line_before = text[min_col - offset: max_col - offset]
-        chars_same_line = text[min_col:max_col]
-        chars_line_after = text[min_col + offset: max_col + offset]
-        chars = {*chars_line_before, *chars_same_line, *chars_line_after}
+def qa_results(lines: list[str], safe_numbers: list[int, int, int]):
+    # # add points around to avoid out of bounds when looking around digits
+    # # pprint.pp(lines)
+    # wrapped_lines = ["." + line.strip() + "." for line in lines]
+    # line_length = len(wrapped_lines[0])
+    # text = "." * line_length + "\n" + "\n".join(wrapped_lines) + "\n" + "." * line_length
+    # # pprint.pp(wrapped_lines)
+    # # pprint.pp(text)
+    # current_index = 0
+    # for number_id, (number, line_index, char_index) in enumerate(safe_numbers):
+    #     sub = str(number)
+    #     # i = text.find(sub, current_index, len(text))
+    #     i = line_length * (line_index + 1) + char_index + 1  # taking into account wrapped points
+    #     min_col = i - 1
+    #     max_col = i + len(sub) + 1  # excluded
+    #     # print(f"{number_id=}, {number=}, {i=}, {min_col=}, {max_col=}")
+    #     offset = line_length + 1  # we have to count newline char now
+    #     chars_line_before = text[min_col - offset: max_col - offset]
+    #     chars_same_line = text[min_col:max_col]
+    #     chars_line_after = text[min_col + offset: max_col + offset]
+    #     chars = {*chars_line_before, *chars_same_line, *chars_line_after}
+    #     symbols = {char for char in chars if char != "." and not char.isdigit()}
+    #     print(line_index, char_index)
+    #     print(f"{number=}, {i=},  {number_id=}, {chars=}")
+    #     print(chars_line_before)
+    #     print(chars_same_line)
+    #     print(chars_line_after)
+    #     if not symbols:
+    #         break
+    #     current_index = i
+    #     # if number_id > 500:
+    #     #     break
+    wrapped_lines = [
+        "." * len(lines[0]),
+        *lines,
+        "." * len(lines[0])
+    ]
+    wrapped_lines = [
+        "." + line + "." for line in wrapped_lines
+    ]
+    for number, line_index, last_char_index in safe_numbers:
+        line_before = wrapped_lines[line_index][last_char_index - len(str(number)): last_char_index + 2]
+        line_of_number = wrapped_lines[line_index + 1][last_char_index - len(str(number)): last_char_index + 2]
+        line_after = wrapped_lines[line_index + 2][last_char_index - len(str(number)): last_char_index + 2]
+        # print(line_before)
+        # print(line_of_number)
+        # print(line_after)
+        # if line_index > 1:
+        #     return
+        chars = {*line_before, *line_after, *line_of_number}
         symbols = {char for char in chars if char != "." and not char.isdigit()}
+        print(f"{number=}, {line_index=}, {last_char_index=}, ")
         if not symbols:
-            print(chars_line_before)
-            print(chars_same_line)
-            print(chars_line_after)
-            print()
+            print(line_before)
+            print(line_of_number)
+            print(line_after)
             break
-        else:
-            print(f"{number=}, {i=}, {number_id=}, {chars=}")
-        current_index = i
-        # if number_id > 500:
-        #     break
+
 
 
 if __name__ == "__main__":
