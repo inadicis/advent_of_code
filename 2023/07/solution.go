5df67e370ea42f911d779b3bce7cd919f1b4bdb9
Inadicis <nilsdaniel.cichy@gmail.com>
2023-12-07 13:04:19 +0100

    implement solution for day 7 part 1

--- 2023/07/solution.go
+++ 2023/07/solution.go
@@ -3,6 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 func main() {
@@ -10,10 +15,102 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// s := "Hello World"
+	// for i, r := range s {
+	// 	fmt.Printf("Range %d, type:%T, r= %#v, r=%d \n", i, r, r, r)
+	// }
+	// for i := 0; i < len(s); i++ {
+	// 	fmt.Printf("index accessing hello[%d], type: %T v=%#v, r=%d \n", i, s[i], s[i], s[i])
+	// }
+
 	fmt.Println(r)
 }
 
-func getResult(filename string) (int, error) {
+type SortByHand []string
 
+func (a SortByHand) Len() int           { return len(a) }
+func (a SortByHand) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a SortByHand) Less(i, j int) bool { return a[i] < a[j] }
+
+func compareHands(line1 string, line2 string) int {
+	hand1, _, _ := strings.Cut(line1, " ")
+	hand2, _, _ := strings.Cut(line2, " ")
+	primary1, secondary1 := getHandValue(hand1)
+	primary2, secondary2 := getHandValue(hand2)
+	primaryDiff := primary1 - primary2
+	if primaryDiff != 0 {
+		return primaryDiff
+	}
+	secondaryDiff := secondary1 - secondary2
+	if secondaryDiff != 0 {
+		return secondaryDiff
+	}
+	cardValues := map[rune]int{
+		'A': 14,
+		'K': 13,
+		'Q': 12,
+		'J': 11,
+		'T': 10,
+	}
+	for i, card1 := range hand1 {
+		card2 := hand2[i]
+		cardDiff := getCardValue(card1, cardValues) - getCardValue(rune(card2), cardValues)
+		// GOLANG? why iterate over a string provides runes (int32), but accessing an index provides a byte (uint8)?
+		if cardDiff != 0 {
+			return cardDiff
+		}
+	}
 	return 0
+}
+
+func getCardValue(card rune, cardValues map[rune]int) int {
+	if unicode.IsDigit(card) {
+		return int(card - '0')
+	}
+	return cardValues[card]
+}
+
+func getHandValue(hand string) (int, int) {
+	counts := map[rune]int{}
+	for _, card := range hand {
+		counts[card]++
+	}
+	primary, secondary := 0, 0
+	for _, count := range counts {
+		if count > primary {
+			secondary = primary
+			primary = count
+		} else if count > secondary {
+			secondary = count
+		}
+	}
+	return primary, secondary
+
+}
+
+func getResult(filename string) (int, error) {
+	// A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, or 2
+	// 	32T3K 765
+	// T55J5 684
+	// KK677 28
+	// KTJJT 220
+	// QQQJA
+	text, err := os.ReadFile(filename)
+	if err != nil {
+		return 0, err
+	}
+	handsLines := strings.Split(strings.TrimSpace(string(text)), "\r\n")
+	slices.SortFunc(handsLines, compareHands)
+
+	total := 0
+	for i, line := range handsLines {
+		_, bidStr, _ := strings.Cut(line, " ")
+		bid, err := strconv.Atoi(bidStr)
+		if err != nil {
+			return 0, err
+		}
+		total += bid * (i + 1)
+	}
+
+	return total, nil
 }
---
+++ 2023/07/solution_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSolution(t *testing.T) {
+	result, err := getResult("test_data.txt")
+	if err != nil {
+		t.Errorf("Function failed %s", err)
+	}
+	expected := 6440
+	if result != expected {
+		t.Errorf("Expected %d, got %d", expected, result)
+	}
+}
