e9954c4c8700f8db3c688b22f4737f24ddac2bb0
Inadicis <nilsdaniel.cichy@gmail.com>
2023-12-08 12:46:44 +0100

    wip implement part 2 - find out why it does not work
     -> copy does not work as expected

--- 2023/08/solution.go
+++ 2023/08/solution.go
@@ -9,14 +9,14 @@ import (
 )
 
 func main() {
-	r, err := getResult("input.txt")
+	r, err := getResultPart2("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(r)
 }
 
-func getResult(filename string) (int, error) {
+func getResultPart1(filename string) (int, error) {
 	text, err := os.ReadFile(filename)
 	if err != nil {
 		return 0, err
@@ -62,6 +62,81 @@ func getResult(filename string) (int, error) {
 		steps++
 
 		if currentNode == "ZZZ" {
+			return steps, nil
+		}
+
+		currentInstructionIndex++
+		if currentInstructionIndex >= len(instructionsLine) {
+			currentInstructionIndex = currentInstructionIndex % len(instructionsLine)
+		}
+
+	}
+
+}
+
+func getResultPart2(filename string) (int, error) {
+	text, err := os.ReadFile(filename)
+	if err != nil {
+		return 0, err
+	}
+
+	instructionsLine, nodesBlock, found := strings.Cut(strings.TrimSpace(string(text)), "\r\n\r\n")
+	if !found {
+		return 0, errors.New("Could not split file into instructions / nodes")
+	}
+
+	// fmt.Println(instructionsLine)
+	// fmt.Println("----")
+	// fmt.Println(nodesBlock)
+	startNodes := []string{}
+	nodes := make(map[string][2]string)
+	for i, nodeLine := range strings.Split(nodesBlock, "\r\n") {
+		source, destinations, found := strings.Cut(nodeLine, " = ")
+		if !found {
+			return 0, fmt.Errorf("Could not split line i=%d, = not found.\n line: %s", i, nodeLine)
+		}
+		fmt.Printf("i %d, source %s, destinations %s \n", i, source, destinations)
+		if source[len(source)-1] == 'A' {
+			startNodes = append(startNodes, source)
+			fmt.Printf(" found new start node. Current start nodes: %v\n", startNodes)
+		}
+		nodes[source] = [2]string{destinations[1:4], destinations[6:9]}
+	}
+
+	var currentNodes []string
+	copy(currentNodes, startNodes) // why does copy not work?
+	fmt.Printf("currentNodes %v\n", currentNodes)
+	var currentInstructionIndex int
+	var instruction int
+	var steps int
+	for {
+		looksGood := true
+		if instructionsLine[currentInstructionIndex] == 'R' {
+			instruction = 1
+		} else if instructionsLine[currentInstructionIndex] == 'L' {
+			instruction = 0 // could be removed but I want the defensive last else
+		} else {
+			return 0, fmt.Errorf("Instruction not parsable at i: %d", currentInstructionIndex)
+		}
+		steps++
+
+		fmt.Printf("step %d, currentNodes %v", steps, currentNodes)
+		for i, currentNode := range currentNodes {
+			fmt.Printf("  i %d, currentNode %s", i, currentNode)
+			possibleNodes, exists := nodes[currentNode]
+			if !exists {
+				return 0, fmt.Errorf("No node found for %s", currentNode)
+			}
+
+			fmt.Printf("Step %d, instructionI %d, instruction: %d, possibleNodes %v", steps, currentInstructionIndex, instruction, possibleNodes)
+			currentNode = possibleNodes[instruction]
+
+			if currentNode[len(currentNode)-1] != 'Z' {
+				looksGood = false
+			}
+			currentNodes[i] = currentNode
+		}
+		if looksGood {
 			return steps, nil
 		}
 
--- 2023/08/solution_test.go
+++ 2023/08/solution_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestSolution2(t *testing.T) {
-	result, err := getResult("test_data2.txt")
+	result, err := getResultPart1("test_data2.txt")
 	if err != nil {
 		t.Errorf("Function failed %s", err)
 	}
@@ -16,7 +16,18 @@ func TestSolution2(t *testing.T) {
 }
 
 func TestSolution6(t *testing.T) {
-	result, err := getResult("test_data6.txt")
+	result, err := getResultPart1("test_data6.txt")
+	if err != nil {
+		t.Errorf("Function failed %s", err)
+	}
+	expected := 6
+	if result != expected {
+		t.Errorf("Expected %d, got %d", expected, result)
+	}
+}
+
+func TestSolutionPart2(t *testing.T) {
+	result, err := getResultPart2("test_data_part2.txt")
 	if err != nil {
 		t.Errorf("Function failed %s", err)
 	}
