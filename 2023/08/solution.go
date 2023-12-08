afcdabec20ee2818d0225e585999b965f46d30e3
Inadicis <nilsdaniel.cichy@gmail.com>
2023-12-08 14:01:10 +0100

    wip implement cached get next exit, now need a heap of current positions

--- 2023/08/solution.go
+++ 2023/08/solution.go
@@ -74,7 +74,66 @@ func getResultPart1(filename string) (int, error) {
 
 }
 
+func isExit(node string) bool {
+	return node[len(node)-1] == 'Z'
+}
+
+func calculateNextExit(position Position, instructions string, nodesMap map[string][2]string) (NextExit, error) {
+	var instruction int
+	if instructions[position.instructionIndex] == 'R' {
+		instruction = 1
+	} else if instructions[position.instructionIndex] != 'L' {
+		return NextExit{}, fmt.Errorf("Instruction not parsable at i: %d", position.instructionIndex)
+	}
+
+	currentNode := position.node
+	var steps int
+	for ; !isExit(currentNode); steps++ {
+		// fmt.Printf("  i %d, currentNode %s\n", i, currentNode)
+		possibleNodes, exists := nodesMap[currentNode]
+		if !exists {
+			return NextExit{}, fmt.Errorf("No node found for %s", currentNode)
+		}
+
+		// fmt.Printf("Step %d, instructionI %d, instruction: %d, possibleNodes %v\n", steps, currentInstructionIndex, instruction, possibleNodes)
+		currentNode = possibleNodes[instruction]
+	}
+	return NextExit{distance: steps, node: currentNode}, nil
+}
+
+type NextExit struct {
+	distance int
+	node     string
+}
+
+type Position struct {
+	instructionIndex int
+	node             string
+}
+
+func InitCachedGetNextExit(instructions string, nodesMap map[string][2]string) func(position Position) (NextExit, error) {
+	var cache = make(map[Position]NextExit)
+	return func(position Position) (NextExit, error) {
+		if retrieved, ok := cache[position]; ok {
+			fmt.Println("from cache")
+			return retrieved, nil
+		}
+		r, err := calculateNextExit(position, instructions, nodesMap)
+		if err != nil {
+			return NextExit{}, err
+		}
+		cache[position] = r
+		return r, nil
+	}
+
+}
+
 func getResultPart2(filename string) (int, error) {
+	// needs at least minutes to find solutions
+	// optimization possibilities:
+	// - find cycles for each source, for each cycle the index of possible destinations (end with Z)
+	// 	 once all cycles found -> calculate equation solution
+	// - define cached function )nextExit(sourceNode string, instructions string, index int)(steps int, exit string
 	text, err := os.ReadFile(filename)
 	if err != nil {
 		return 0, err
@@ -95,17 +154,24 @@ func getResultPart2(filename string) (int, error) {
 		if !found {
 			return 0, fmt.Errorf("Could not split line i=%d, = not found.\n line: %s", i, nodeLine)
 		}
-		fmt.Printf("i %d, source %s, destinations %s \n", i, source, destinations)
+		// fmt.Printf("i %d, source %s, destinations %s \n", i, source, destinations)
 		if source[len(source)-1] == 'A' {
 			startNodes = append(startNodes, source)
-			fmt.Printf(" found new start node. Current start nodes: %v\n", startNodes)
+			// fmt.Printf(" found new start node. Current start nodes: %v\n", startNodes)
 		}
 		nodes[source] = [2]string{destinations[1:4], destinations[6:9]}
 	}
 
-	var currentNodes []string
-	copy(currentNodes, startNodes) // why does copy not work?
-	fmt.Printf("currentNodes %v\n", currentNodes)
+	getNextExit := InitCachedGetNextExit(instructionsLine, nodes)
+
+	// fmt.Printf("startNodes %v\n", startNodes)
+	currentPositions := make([]Position, len(startNodes))
+	for i, node := range startNodes {
+		currentPositions[i] = Position{instructionIndex: 0, node: node}
+	}
+	amountCopied := copy(currentNodes, startNodes) // why does copy not work?
+	fmt.Printf(" amount copied: %d\n", amountCopied)
+	// fmt.Printf("currentNodes %v\n", currentNodes)
 	var currentInstructionIndex int
 	var instruction int
 	var steps int
@@ -119,16 +185,17 @@ func getResultPart2(filename string) (int, error) {
 			return 0, fmt.Errorf("Instruction not parsable at i: %d", currentInstructionIndex)
 		}
 		steps++
-
-		fmt.Printf("step %d, currentNodes %v", steps, currentNodes)
+		if steps%1000000 == 0 {
+			fmt.Printf("step %d, currentNodes %v\n", steps, currentNodes)
+		}
 		for i, currentNode := range currentNodes {
-			fmt.Printf("  i %d, currentNode %s", i, currentNode)
+			// fmt.Printf("  i %d, currentNode %s\n", i, currentNode)
 			possibleNodes, exists := nodes[currentNode]
 			if !exists {
 				return 0, fmt.Errorf("No node found for %s", currentNode)
 			}
 
-			fmt.Printf("Step %d, instructionI %d, instruction: %d, possibleNodes %v", steps, currentInstructionIndex, instruction, possibleNodes)
+			// fmt.Printf("Step %d, instructionI %d, instruction: %d, possibleNodes %v\n", steps, currentInstructionIndex, instruction, possibleNodes)
 			currentNode = possibleNodes[instruction]
 
 			if currentNode[len(currentNode)-1] != 'Z' {
