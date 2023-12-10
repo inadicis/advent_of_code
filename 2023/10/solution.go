66c5f235fbafe01258e4e39b1cb3edf41fb16130
Inadicis <nilsdaniel.cichy@gmail.com>
2023-12-10 17:44:15 +0100

    wip implement area calculator for day10 part 2

--- 2023/10/solution.go
+++ 2023/10/solution.go
@@ -48,7 +48,7 @@ var pipeToRune map[Pipe]rune = map[Pipe]rune{
 }
 
 func getExitDirection(currentPipe rune, entranceDirection Direction) (exit Direction, found bool) {
-	fmt.Printf(" getExit(%s, direction: %v)", currentPipe, entranceDirection)
+	// fmt.Printf(" getExit(%s, direction: %v\n)", currentPipe, entranceDirection)
 	reversedDirection := Direction{-entranceDirection.row, -entranceDirection.col}
 	pipe, found := runeToPipe[currentPipe]
 	if !found {
@@ -88,7 +88,7 @@ type Tile struct {
 func BreadthFirstSearch(maze [][]rune, startPosition Position) (furthestTile Tile, visited map[Position]Tile, startPipe rune) {
 	// could have just iterated over one way and come back to start
 	// (as there are only 2 valid adjacent tiles to start this must work)
-	// visited := make(map[Position]int)
+	visited = make(map[Position]Tile)
 	// queue := make([]Position, 4)
 	queue := list.New()
 	for _, direction := range []Direction{up, right, down, left} {
@@ -115,7 +115,12 @@ func BreadthFirstSearch(maze [][]rune, startPosition Position) (furthestTile Til
 			direction2 := currentQueuedTile.origin
 			startRune, found := pipeToRune[Pipe{direction1, direction2}]
 			if !found {
-				panic("No pipe found matching directions given")
+				startRune, found = pipeToRune[Pipe{direction2, direction1}]
+				if !found {
+					fmt.Printf("direction1 %#v, direction2 %#v matches no rune", direction1, direction2)
+					panic("No pipe found matching directions given")
+				}
+
 			}
 			return currentQueuedTile, visited, startRune
 		}
@@ -126,33 +131,33 @@ func BreadthFirstSearch(maze [][]rune, startPosition Position) (furthestTile Til
 		if found {
 			nextPosition, exists := getNextPosition(maze, currentQueuedTile.position, exitDirection)
 			if exists {
-				queue.PushBack(Tile{position: nextPosition, entranceDirection: exitDirection, distance: currentQueuedTile.distance + 1})
+				queue.PushBack(Tile{position: nextPosition, entranceDirection: exitDirection, distance: currentQueuedTile.distance + 1, origin: currentQueuedTile.origin})
 				visited[currentQueuedTile.position] = currentQueuedTile
 			} else {
 				fmt.Printf("step %d: Stopped branch ending with %#v as it pointed outside the maze", i, currentQueuedTile)
 			}
 
 		} else {
-			fmt.Printf("step %d: Stopped branch ending with %#v as its position was not recognized as a pipe", i, currentQueuedTile)
+			fmt.Printf("step %d: Stopped branch ending with %#v as its position was not recognized as a pipe\n", i, currentQueuedTile)
 
 		}
 
 	}
 }
 
-func getBorders(maze [][]rune, startPosition Position) [][]rune {
-	// initialize maze with only empty string tiles,
-	// then fill only the tiles that are part of the cycle
-	cleanMaze := make([][]rune, len(maze))
-	for i, row := range maze {
-		cleanMaze[i] = make([]rune, len(row))
-	}
-	currentPosition := startPosition
-	for {
+// func getBorders(maze [][]rune, startPosition Position) [][]rune {
+// 	// initialize maze with only empty string tiles,
+// 	// then fill only the tiles that are part of the cycle
+// 	cleanMaze := make([][]rune, len(maze))
+// 	for i, row := range maze {
+// 		cleanMaze[i] = make([]rune, len(row))
+// 	}
+// 	currentPosition := startPosition
+// 	for {
 
-	}
+// 	}
 
-}
+// }
 
 // func findStartPos(maze [][]rune)Position{
 // 	for i, row := range maze {
@@ -172,7 +177,51 @@ func main() {
 	// fmt.Print(getExitDirection('L', down))
 	furthestTile, visitedTiles, startRune := BreadthFirstSearch(maze, startPos)
 	fmt.Printf("Furtherst Position found at distance %d: %#v ", furthestTile.distance, furthestTile.position)
+	// maze[startPos.row][startPos.col] = startRune
+	visitedTiles[startPos] = Tile{position: startPos, distance: 0, origin: furthestTile.origin}
+	area := countEnclosedTiles(maze, visitedTiles, startPos, startRune)
+	fmt.Println(area)
+}
 
+func countEnclosedTiles(maze [][]rune, visitedTiles map[Position]Tile, startPos Position, startRune rune) (amount int) {
+	// go through each tile in each row (horizontally). Check for each tile if it is "in" the circle.
+	// for the current position, we can either be in the circle, at its edge (on a pipe), or outside.
+	// We only get "in" the circle by "crossing" a pipe on the edge of the circle.
+	// So we simply store if we are currently "in" the circle, and alternate this value everytime we "cross" the edge
+	// and count how many tiles are in
+
+	// to "enter" the area, each line has to "cross" a up-down | pipe.
+	// this pipe could be "prolonged/divided" in multiple L--7 pipes or similar,
+	// but the one extreme must be up, the other must be down to inverse In/Out (LJ would just be ignored)
+	acceptedOrigins := runeToPipe[startRune]
+	for i, row := range maze {
+		isIn := false
+		crossedUp := false
+		crossedDown := false
+		for j, r := range row {
+			tile, visited := visitedTiles[Position{i, j}]
+			if visited && (tile.origin == acceptedOrigins.entrance || tile.origin == acceptedOrigins.exit) {
+				switch {
+				case r == '|':
+					isIn = !isIn
+				case r == 'J' || r == 'L':
+					crossedUp = !crossedUp
+				case r == 'F' || r == '7':
+					crossedDown = !crossedDown
+				}
+				if crossedDown && crossedUp {
+					isIn = !isIn
+					crossedDown = false
+					crossedUp = false
+				}
+
+			} else if isIn {
+				amount++
+			}
+
+		}
+	}
+	return amount
 }
 
 func ExtractData(filename string) ([][]rune, Position, error) {
@@ -189,10 +238,10 @@ func ExtractData(filename string) ([][]rune, Position, error) {
 			row[j] = rune(char)
 			if row[j] == 'S' {
 				startPos = Position{row: i, col: j}
-				fmt.Printf("Found Start position %#v\n", startPos)
+				// fmt.Printf("Found Start position %#v\n", startPos)
 			}
 		}
-		fmt.Println(row)
+		// fmt.Println(row)
 		rows[i] = row
 	}
 	return rows, startPos, nil
