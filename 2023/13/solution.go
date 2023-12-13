495fc9cb143a4374c0cc8bcc280214a19e68a1a8
Nils <nilsdaniel.cichy@gmail.com>
2023-12-13 11:55:34 +0100

    implement day 13 part 1

--- 2023/13/solution.go
+++ 2023/13/solution.go
@@ -8,8 +8,8 @@ import (
 )
 
 func main() {
-	filename := "test_data.txt"
-	// filename := "input.txt"
+	// filename := "test_data.txt"
+	filename := "input.txt"
 	data, err := ExtractData(filename)
 
 	if err != nil {
@@ -23,36 +23,57 @@ func main() {
 	fmt.Printf("\nFinal result: %d", result)
 }
 
-func ExtractData(filename string) (lines []string, err error) {
-	block, err := os.ReadFile(filename)
+func ExtractData(filename string) (patterns [][]string, err error) {
+	input, err := os.ReadFile(filename)
 	if err != nil {
-		return lines, err
+		return patterns, err
 	}
-	lines = strings.Split(strings.TrimSpace(string(block)), "\r\n")
-	// rowsMasks := make([]string, len(lines))
-	// rowsAmounts := make([][]int, len(lines))
-	// for i, line := range lines {
-	// 	maskLine, amountsLine, found := strings.Cut(line, " ")
-	// 	if !found {
-	// 		return rowsMasks, rowsAmounts, fmt.Errorf("No space found at %d", i)
-	// 	}
-	// 	amountSplit := strings.Split(amountsLine, ",")
-	// 	amounts := make([]int, len(amountSplit))
-	// 	for j, amount := range amountSplit {
-	// 		a, err := strconv.Atoi(amount)
-	// 		if err != nil {
-	// 			return rowsMasks, rowsAmounts, err
-	// 		}
-	// 		amounts[j] = a
-	// 	}
+	blocks := strings.Split(strings.TrimSpace(string(input)), "\r\n\r\n")
+	patterns = make([][]string, len(blocks))
+	for i, block := range blocks {
+		patterns[i] = strings.Split(block, "\r\n")
+	}
 
-	// 	rowsMasks[i] = maskLine
-	// 	rowsAmounts[i] = amounts
-	// }
-	return lines, nil
+	return patterns, nil
 }
 
-func ProcessData(lines []string) (total int, err error) {
-
+func ProcessData(blocks [][]string) (total int, err error) {
+	for i, block := range blocks {
+		fmt.Printf("%03d: Processing %v\n", i, block)
+		row, col := findSymmetry(block)
+		total += col + 100*row
+	}
 	return total, nil
+}
+
+func findSymmetry(block []string) (row int, col int) {
+COLAXIS:
+	for colAxis := 1; colAxis < len(block[0]); colAxis++ {
+		for _, row := range block {
+			// fmt.Printf(" Comparing row %d: %q with axis %d", i, row, colAxis)
+			distanceToEdge := min(colAxis, len(row)-colAxis)
+			for j := 0; j < distanceToEdge; j++ {
+				// fmt.Printf("  %q ?= %q", row[colAxis-j-1], row[colAxis+j])
+				if row[colAxis-j-1] != row[colAxis+j] {
+					// fmt.Printf("   FALSE -> skip axis %d", colAxis)
+					continue COLAXIS
+				}
+			}
+		}
+		col = colAxis
+	}
+
+ROWAXIS:
+	for rowAxis := 1; rowAxis < len(block); rowAxis++ {
+		for i := 0; i < len(block[0]); i++ {
+			distanceToEdge := min(rowAxis, len(block)-rowAxis)
+			for j := 0; j < distanceToEdge; j++ {
+				if block[rowAxis-j-1][i] != block[rowAxis+j][i] {
+					continue ROWAXIS
+				}
+			}
+		}
+		row = rowAxis
+	}
+	return row, col
 }
