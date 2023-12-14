5f098a0e8d0e1b9ed2a6220d3a487e2c33da3cd8
Nils <nilsdaniel.cichy@gmail.com>
2023-12-14 15:29:55 +0100

    write a generic tilt + refactor cycle detection

--- 2023/14/solution.go
+++ 2023/14/solution.go
@@ -47,28 +47,18 @@ func part2(input string) int {
 
 	amountCycles := 1000000000
 	foundCycle := false
-	rest := amountCycles
-	for i := 0; i < amountCycles; i++ {
-		if i%1000 == 0 {
-			fmt.Println(i)
-		}
+	for i := amountCycles; i > 0; i-- {
 		hash := encode(rows)
-		if j, ok := visited[hash]; !foundCycle && ok {
-			cycleLength := i - j
-			rest = (amountCycles - i) % cycleLength
-			foundCycle = true
-			// fmt.Printf(" %05d: Found cycle of length %d starting at i: %d. %d steps left\n", i, cycleLength, j, rest)
-			// TODO
-		}
-		if rest <= 0 {
-			break
-		}
 		if !foundCycle {
-			visited[encode(rows)] = i
+			if j, ok := visited[hash]; ok {
+				cycleLength := i - j
+				i = i % cycleLength
+				foundCycle = true
+			} else {
+				visited[hash] = i
+			}
 		}
 		spinRocks(rows)
-		rest--
-
 	}
 	return countWeight(rows)
 }
@@ -111,130 +101,46 @@ func encode(a [][]rune) string {
 	return buffer.String()
 }
 
-func decode(s string) [][]rune {
-	splitLines := strings.Split(s, "\n")
-	a := make([][]rune, len(splitLines))
-	for i, l := range splitLines {
-		r := make([]rune, len(l))
-		for j, char := range l {
-			r[j] = char
-		}
-		a[i] = r
-	}
-	return a
-}
-
 func spinRocks(lines [][]rune) {
-	// for _, dir := range []Direction{1, 2, 3, 4} {
-	// 	fmt.Println(dir)
-	// }
-	// NORTH
-	tiltNorthSouth(lines, false)
-	// WEST
-	tiltWestEast(lines, false)
-	// SOUTH
-
-	tiltNorthSouth(lines, true)
-	// EAST
-	tiltWestEast(lines, true)
+	// NORTH WEST SOUTH EAST
+	tilt(lines, true, false)
+	tilt(lines, false, false)
+	tilt(lines, true, true)
+	tilt(lines, false, true)
 }
 
-func tiltNorthSouth(lines [][]rune, south bool) {
-
-	for col := 0; col < len(lines[0]); col++ {
-		initialRow := 0
-		lastRow := len(lines) - 1
+func tilt(lines [][]rune, upDown bool, reverse bool) {
+	// generic tilt, but harder to understand
+	firstIter := len(lines[0])
+	secondIter := len(lines)
+	if !upDown {
+		firstIter, secondIter = secondIter, firstIter
+	}
+	for i := 0; i < firstIter; i++ {
+		initialJ := 0
+		lastJ := secondIter - 1
 		step := 1
-		if south {
-			initialRow, lastRow = lastRow, initialRow
+		if reverse {
+			initialJ, lastJ = lastJ, initialJ
 			step = -1
 		}
-		potentialRow := initialRow
-		for row := initialRow; (lastRow-row)*step >= 0; row += step {
+		potentialJ := initialJ
+		for j := initialJ; (lastJ-j)*step >= 0; j += step {
+			row, col := j, i
+			potentialRow, potentialCol := potentialJ, col
+			if !upDown {
+				row, col = col, row
+				potentialRow, potentialCol = row, potentialJ
+			}
 			char := lines[row][col]
-			if char == '#' {
-				potentialRow = row + step
-			} else if char == 'O' {
+			switch {
+			case char == '#':
+				potentialJ = j + step
+			case char == 'O':
 				lines[row][col] = '.'
-				lines[potentialRow][col] = 'O'
-				potentialRow += step
-			} else if char != '.' { // do nothing on points
-				panic("unexpected rune")
+				lines[potentialRow][potentialCol] = 'O'
+				potentialJ += step
 			}
 		}
 	}
 }
-func tiltWestEast(lines [][]rune, east bool) {
-
-	for row := 0; row < len(lines[0]); row++ {
-		initialCol := 0
-		lastCol := len(lines[0]) - 1
-		step := 1
-		if east {
-			initialCol, lastCol = lastCol, initialCol
-			step = -1
-		}
-		potentialCol := initialCol
-		for col := initialCol; (lastCol-col)*step >= 0; col += step {
-			char := lines[row][col]
-			if char == '#' {
-				potentialCol = col + step
-			} else if char == 'O' {
-				lines[row][col] = '.'
-				lines[row][potentialCol] = 'O'
-				potentialCol += step
-			} else if char != '.' { // do nothing on points
-				panic("unexpected rune")
-			}
-		}
-	}
-}
-
-func pprint(lines [][]rune) {
-	for _, row := range lines {
-		for _, char := range row {
-			fmt.Printf("%c ", char)
-		}
-		fmt.Println()
-	}
-}
-
-// func tiltRocks(lines [][]rune, upDown bool, reverse bool) {
-
-// 	firstIter := lines
-// 	secondIter := lines[0]
-// 	if upDown {
-// 		firstIter, secondIter := secondIter, firstIter
-// 	}
-
-// 	for i := 0; i < len(firstIter); i++ {
-// 		potentialPosition := 0
-// 		for j := 0; j < len(secondIter); j++ {
-// 			if lines[row][col] == '#' {
-// 				potentialPosition = row - 1
-// 			} else if lines[row][col] == 'O' {
-// 				lines[potentialPosition][col] = 'O'
-// 				potentialPosition--
-// 			} else if lines[row][col] != '.' { // do nothing on points
-// 				panic("unexpected rune")
-// 			}
-// 		}
-// 	}
-
-// }
-
-// type Direction int
-
-// const (
-// 	NORTH Direction = iota + 1
-// 	WEST
-// 	SOUTH
-// 	EAST
-// )
-
-// func tiltRocks(lines []string, direction Direction) {
-// 	switch {
-// 	case direction == NORTH:
-
-// 	}
-// }
