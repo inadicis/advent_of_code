e4f568458e52928360d38b6e3895a9f42701cbd9
Nils Cichy <nils.cichy@check24.de>
2024-12-02 15:18:09 +0100

    naive solution for part 2 of 2024-02

--- 2024/02/solution.py
+++ 2024/02/solution.py
@@ -1,41 +1,58 @@
+def find_conflicts(numbers: list[int]) -> list[int]:
+    """
+    returns the indexes of the numbers that conflict with the previous one,
+    assuming the first two numbers define the order
+    """
+
+    first_diff = numbers[1] - numbers[0]
+    first_is_descending = first_diff < 0
+    previous = numbers[0]
+    conflicts = []
+    for index, number in enumerate(numbers[1:]):
+        current_diff = number - previous
+        current_is_descending = current_diff < 0
+        if (
+            current_is_descending != first_is_descending
+            or abs(current_diff) > 3
+            or current_diff == 0
+        ):
+            conflicts.append(index)
+        previous = number
+    return conflicts
+
+
 def main(file_name: str) -> int:
     valid_lines = []
     invalid_lines = []
 
     with open(file_name) as f:
         for line in f.readlines():
-            print(line.strip(), end=" - ")
             numbers = [int(i) for i in line.strip().split(" ")]
-            # print(numbers)
-            # is_descending = numbers[1] < numbers[0]
-            first_diff = numbers[1] - numbers[0]
-            first_is_descending = first_diff < 0
-            print(f"{first_is_descending=}", end="")
-            previous = numbers[0]
-            # print(f"{descending=}, {previous=}")
-            for number in numbers[1:]:
-                current_diff = number - previous
-                current_is_descending = current_diff < 0
-                if (
-                    current_is_descending != first_is_descending
-                    or abs(current_diff) > 3
-                    or current_diff == 0
-                ):
-                    invalid_lines.append(line)
-                    print(" -- INVALID", f"conflict: {previous}, {number}")
-                    break
-                previous = number
-            else:
-                valid_lines.append(line)
-                print(" -- is valid")
+            conflicts = find_conflicts(numbers)
 
-    # print("Valid lines:")
-    # for line in invalid_lines:
-    #     print(line)
-    print(len(valid_lines))
+            # if len(conflicts) > 3:
+            #     # cannot fix with only one removal for sure
+            #     # (at least 2 independent pairs of involved numbers,
+            #     # removing one would fix maximum 1 pair)
+            #     invalid_lines.append(line)
+            # elif len(conflicts) == 0:
+            #     # with 0 conflict, obviously valid
+            #     valid_lines.append(line)
+            # else:
+            if find_conflicts(numbers) == []:
+                valid_lines.append(line)
+                continue
+
+            for index, _ in enumerate(numbers):
+                if find_conflicts(numbers[:index] + numbers[index + 1 :]) == []:
+                    valid_lines.append(line)
+                    break
+            else:
+                invalid_lines.append(line)
+
     return len(valid_lines)
 
 
 if __name__ == "__main__":
     # main("test_input.txt")
-    main("input.txt")
+    print(main("input.txt"))
